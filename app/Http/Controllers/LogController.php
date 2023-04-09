4bbc2e9006fea5277b140e223909f5e473b1e22a
Rossi <syafi.arrosyid@gmail.com>
2023-04-09 16:32:11 +0700

    fix logs

--- app/Http/Controllers/LogController.php
+++ app/Http/Controllers/LogController.php
@@ -12,7 +12,7 @@ class LogController extends Controller
         $model_type = request('model_type');
         $model_id = request('model_id');
         // dd(Logs::with(['user', 'target'])->where('model_type', $model_type)->where('model_id', $model_id)->get());
-        $logs = Logs::with(['user', 'target'])->has('user')->has('target')->where('model_type', $model_type)->where('model_id', $model_id)->get();
+        $logs = Logs::with(['user', 'target'])->where('model_type', $model_type)->where('model_id', $model_id)->get();
         $data = LogResource::collection($logs);
         return response()->json($data);
     }
--- app/Http/Resources/LogResource.php
+++ app/Http/Resources/LogResource.php
@@ -16,12 +16,12 @@ class LogResource extends JsonResource
     {
         // dd($this->user->name);
         return [
-            'user' => $this->user->name,
-            'foto' => $this->user->foto(),
+            'user' => $this->user ? $this->user->name : "-",
+            'foto' => $this->user ? $this->user->foto() :  asset('/dist/img/businessman.png'),
             'target' => optional($this->target)->name,
             'action' => badgeApproval($this->action),
             // 'action' => $this->action,
-            'tanggal' => date('d-m-Y  H:i', strtotime($this->created_at))
+            'tanggal' => date('d/m/Y  H:i', strtotime($this->created_at))
         ];
     }
 }
