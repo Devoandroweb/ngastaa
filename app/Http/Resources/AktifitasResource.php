752b121d8b78a22a532ed827e08c71cbe4e61f9f
Dev'o Web-Andro <55377597+Devoandroweb@users.noreply.github.com>
2024-02-03 07:29:06 +0700

    fix api aktifitas

--- app/Http/Resources/AktifitasResource.php
+++ app/Http/Resources/AktifitasResource.php
@@ -17,8 +17,8 @@ class AktifitasResource extends JsonResource
         return [
             'nama' => $this->pegawai->getFullName(),
             'koordinat' => $this->koordinat,
-            'jabatan' => $this->pegawai->riwayat_jabatan?->first()->tingkat?->first()->nama,
-            'divisi' => $this->pegawai->riwayat_jabatan?->first()->skpd?->first()->nama,
+            'jabatan' => $this->pegawai->riwayat_jabatan?->first()?->tingkat?->first()->nama,
+            'divisi' => $this->pegawai->riwayat_jabatan?->first()?->skpd?->first()->nama,
             'foto' => url('public/'.$this->foto),
             'created_at' => hari(date("w",strtotime($this->created_at))).", ".tanggal_indo($this->created_at),
             'dimulai_pada' => date("H:i:s",strtotime($this->created_at)),
